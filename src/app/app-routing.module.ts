2c507ae3ce077297045dd2a4f31b5644602136a6
Nitheesh KR <nitheeshkr115@gmail.com>
2022-03-03 10:06:08 +0530

    project folder structure changes for lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 import { ResumeLayoutComponent } from './resume-layout/resume-layout.component';
 
 const routes: Routes = [
-  { path: '', component: PersonalDetailsComponent },
+  {
+    path: '',
+    loadChildren: () => import('./personal-details/personal-details.module').then(m => m.PersonalDetailsModule)
+  },
   { path: 'resume', component: ResumeLayoutComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { AboutMeComponent } from './personal-details/about-me/about-me.component';
-import { ExperienceComponent } from './personal-details/experience/experience.component';
-import { EducationComponent } from './personal-details/education/education.component';
-import { SkillsComponent } from './personal-details/skills/skills.component';
-import { InterestsComponent } from './personal-details/interests/interests.component';
-import { CertificationsComponent } from './personal-details/certifications/certifications.component';
-import { AwardsComponent } from './personal-details/awards/awards.component';
-import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+
 import { ResumeLayoutComponent } from './resume-layout/resume-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    AboutMeComponent,
-    ExperienceComponent,
-    EducationComponent,
-    SkillsComponent,
-    InterestsComponent,
-    CertificationsComponent,
-    AwardsComponent,
-    PersonalDetailsComponent,
     ResumeLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

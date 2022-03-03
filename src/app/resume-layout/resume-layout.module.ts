eec6c7bdab649d36c41aea2fe7bbf95598b1aaff
Nitheesh KR <nitheeshkr115@gmail.com>
2022-03-03 12:59:17 +0530

    integrated projects

--- src/app/resume-layout/resume-layout.module.ts
+++ src/app/resume-layout/resume-layout.module.ts
@@ -5,6 +5,7 @@ import { ResumeLayoutComponent } from './resume-layout.component';
 import { ResumeAboutMeComponent } from './resume-about-me/resume-about-me.component';
 import { ResumeExperienceComponent } from './resume-experience/resume-experience.component';
 import { ResumeEducationComponent } from './resume-education/resume-education.component';
+import { ResumeProjectsComponent } from './resume-projects/resume-projects.component';
 
 
 
@@ -14,6 +15,7 @@ import { ResumeEducationComponent } from './resume-education/resume-education.co
     ResumeAboutMeComponent,
     ResumeExperienceComponent,
     ResumeEducationComponent,
+    ResumeProjectsComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/assets/data/projects.ts
@@ -0,0 +1,55 @@
+const projects = [
+    {
+        id: 0,
+        name: "Tryg",
+        client_location: "Norway",
+        description: "Online solution for creating insurance for pets, home and other items.",
+        technologies: ["VueJS", "VueX", "GraphQL", "Bootstrap 4", "SASS"],
+        responsibilities: [
+            "Developing application interface.",
+            "Unit testing.",
+            "Storybook."
+        ]
+    },
+    {
+        id: 1,
+        name: "LahiTapiola",
+        client_location: "Finland",
+        description: "Online solution for creating life insurance policies and their maintenance.",
+        technologies: ["Angular 8", "Node JS", "MongoDB", "Bootstrap 4", "SASS"],
+        responsibilities: [
+            "Creating application user interface.",
+            "API development with microservice architecture with Node JS and MongoDB.",
+            "Integrating Angular application with custom third party UI components.",
+            "Unit testing."
+        ]
+    },
+    {
+        id: 2,
+        name: "Petronas",
+        client_location: "Malaysia",
+        description: "Web platform to display the statistical analysis of trade in a petroleum based company.",
+        technologies: ["React JS", "Redux", "Redux Saga", "D3.js", "SASS"],
+        responsibilities: [
+            "Creating new React components with Redux Saga, JSS and SASS.",
+            "Created custom chart elements with D3 js.",
+            "Implemented declarative D3 chart.",
+            "Implemented polling using redux saga."
+        ]
+    },
+    {
+        id: 3,
+        name: "HR Application - Web",
+        client_location: "India",
+        description: "Web platform to manage the employee details within an institution.",
+        technologies: ["Angular 6", "SASS", "Bootstrap 4"],
+        responsibilities: [
+            "Developing application UI.",
+            "Developing application architecture",
+            "Integrating application with Bootstrap 4, SASS.",
+            "Deploying the Project in production environment."
+        ]
+    }
+];
+
+export default projects;
